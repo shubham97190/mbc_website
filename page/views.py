86b5737637cc59e31a3d618e29879d2e89eb96d8
nikeshkhandelwal <nikesh.khandelwal@streamlinedcommunications.com>
2025-02-17 22:28:59 -0500

    updates location

--- page/views.py
+++ page/views.py
@@ -141,5 +141,5 @@ class LocationPageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["locations"] = Membership.objects.all()
+        context["locations"] = Location.objects.all()
         return context
